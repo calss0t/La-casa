bba7594c9e7c2157bc1712c7c62798850c2c6bd3
Calss0t <pol.texido.b@gmail.com>
2023-05-24 21:37:42 +0900

    Building date picker for booking form

--- src/Components/TopBlock.tsx
+++ src/Components/TopBlock.tsx
@@ -1,6 +1,13 @@
 import mainImage from "./../images/TopImage.jpg"
 
-import {imageStyle} from "./../styles/TopBlock"
+import React, { useEffect, useState } from "react";
+import Form from 'react-bootstrap/Form';
+import { MDBIcon } from 'mdb-react-ui-kit';
+import moment from "moment";
+import DatePicker from "react-datepicker";
+
+
+import { imageStyle, bookingFormOuter, section, bookingFormInner, Button, buttonText, bookingBoxOuter, bookingBoxInner, bookingTitle } from "./../styles/TopBlock"
 
 /** @jsxRuntime classic */
 /** @jsx jsx */
@@ -16,9 +23,83 @@ type Props = {
 
 
 const TopBlock: React.FC<Props> = (props) => {
+  const [startDate, setStartDate] = useState<string>(moment().format('DD/MM/YYYY'))
+  const [endDate, setEndDate] = useState<string>(moment().add(1, "day").format('DD/MM/YYYY'))
+
+
   return (
-    <div>
-        <img alt='logo' css={imageStyle} src={mainImage} />
+    <div css={section}>
+      <img alt='logo' css={imageStyle} src={mainImage} />
+      <div css={bookingFormOuter}>
+        <div css={bookingFormInner}>
+          <div style={{ width: "100%", display: "flex" }}>
+            <div css={bookingBoxOuter}>
+              <p css={bookingTitle}> Check in </p>
+              <div css={bookingBoxInner} style={{ borderRight: "1px solid #BBC8D4" }}>
+                <MDBIcon icon="calendar-alt" style={{ color: "#6D7D8B", paddingRight: "5px" }} />
+                <DatePicker selected={new Date()} onSelect={()=> console.log(setEndDate)} onChange={(date)=>console.log(date)}/>
+                {/* <Form.Control
+                  placeholder={startDate}
+                  aria-label="Email"
+                  style={{
+                    border: "none",
+                    height: "100%",
+                    width: "100%",
+                    fontFamily: "Montserrat-SemiBold",
+                    fontSize: "14px",
+                    lineHeight: "150.6%",
+                    // color: "#DAE3EA"
+                  }}
+                /> */}
+
+              </div>
+            </div>
+            <hr />
+            <div css={bookingBoxOuter} >
+              <p css={bookingTitle}> Check out </p>
+              <div css={bookingBoxInner} style={{ borderRight: "1px solid #BBC8D4" }}>
+                <MDBIcon icon="calendar-alt" style={{ color: "#6D7D8B", paddingRight: "5px" }} />
+                <Form.Control
+                  placeholder={endDate}
+                  aria-label="Email"
+                  style={{
+                    border: "none",
+                    height: "100%",
+                    width: "100%",
+                    fontFamily: "Montserrat-SemiBold",
+                    fontSize: "14px",
+                    lineHeight: "150.6%",
+                    // color: "#DAE3EA",
+                  }}
+                />
+
+              </div>
+            </div>
+            <hr />
+            <div css={bookingBoxOuter}>
+              <p css={bookingTitle}> Guests </p>
+              <div css={bookingBoxInner}>
+                <MDBIcon icon="user-alt" style={{ color: "#6D7D8B", paddingRight: "5px" }} />
+                <Form.Control
+                  placeholder="Email"
+                  aria-label="Email"
+                  style={{
+                    border: "none",
+                    height: "100%",
+                    width: "100%",
+                    fontFamily: "Montserrat-SemiBold",
+                    fontSize: "14px",
+                    lineHeight: "150.6%",
+                    // color: "#DAE3EA"
+                  }}
+                />
+
+              </div>
+            </div>
+          </div>
+          <Button><p css={buttonText}>Read more</p></Button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/styles/TopBlock.tsx
+++ src/styles/TopBlock.tsx
@@ -3,12 +3,88 @@
 
 
 import { jsx, css, Global, ClassNames } from '@emotion/react'
+import styled from '@emotion/styled'
 
-const imageStyle = css ({
+const Button = styled.button`
+  color: #FFFFFF;
+  background-color: rgba(201, 162, 131, 0.6);
+  border-radius: 4px;
+  width: 165px;
+  height:56px;
+  display: flex;   
+  margin: auto; 
+  text-align: center;
+  align-content: center;
+  align-items: center;
+  border-style : none ;
+`
+
+const buttonText = css({
+    fontFamily: "Montserrat-Medium",
+    margin:"auto",
+    fontSize: "20px",
+    lineHeight: "24px",
+})
+
+const section = css({
+    position: "relative"
+})
+
+const bookingBoxOuter = css({
+    backgroundColor: "white",
+    display:"flex",
+    flexDirection:"column",
+    width:"100%",
+})
+
+const bookingBoxInner = css({
+    display:"flex",
+    padding:"5px",
+    alignItems:"center",
+    marginBottom:"5px"
+})
+
+
+const imageStyle = css({
     width: "100%",
     height: "600px"
 })
 
+const bookingFormOuter= css({
+    position: "relative",
+    top:"-150px",
+    margin:"auto",
+    textAlign:"center",
+    width: "50%",
+    backgroundColor: "white"
+})
+
+const bookingFormInner= css({
+    margin:"auto",
+    padding:"15px",
+    backgroundColor: "blue",
+    display:"flex",
+})
+
+const bookingTitle = css({
+    textAlign:"left",
+    paddingLeft:"25px",
+    fontFamily: "Montserrat-Medium",
+    fontSize: "14px",
+    lineHeight: "150.6%",
+    color:"#5A87A4",
+    paddingTop:"5px",
+    marginBottom:"0px"
+})
+
 export {
-    imageStyle
+    imageStyle,
+    bookingFormOuter,
+    section,
+    bookingFormInner,
+    Button,
+    buttonText,
+    bookingBoxOuter,
+    bookingBoxInner,
+    bookingTitle
 }
